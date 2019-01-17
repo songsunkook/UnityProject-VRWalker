0f7968ce47f8f3bb1d6c3a0c34e8120bc7597dca
songsunkook <songsunkook@gmail.com>
2019-01-18 00:51:15 +0900

    코딩중..

--- Assets/3. Scripts/NextStage.cs
+++ Assets/3. Scripts/NextStage.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextStage : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static void gotoNext()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        char sceneNum = sceneName[sceneName.Length - 1];
+
+        sceneName.Remove(sceneName.Length - 1);
+
+        SceneManager.LoadScene(sceneName + (sceneNum + 1));
+
+    }
 }
