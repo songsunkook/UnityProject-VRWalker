5f56362b91f2ae761ab9d7527202ea4620b4f632
sin <sin010820@naver.com>
2019-01-18 00:55:09 +0900

    Revert "Merge branch 'master' of https://github.com/songsunkook/UnityProject-VRWalker"
    This reverts commit 4eb6bc1cee1266724d8893e79fe8e326a13d8570, reversing
    changes made to e6492c93a5bffa83385f75f4e7ce9151e2ce5d2a.

--- Assets/3. Scripts/NextStage.cs
+++ Assets/3. Scripts/NextStage.cs
@@ -1,19 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class NextStage : MonoBehaviour {
 
-    public static void gotoNext()
-    {
-        string sceneName = SceneManager.GetActiveScene().name;
-
-        char sceneNum = sceneName[sceneName.Length - 1];
-
-        sceneName.Remove(sceneName.Length - 1);
-
-        SceneManager.LoadScene(sceneName + (sceneNum + 1));
-
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
