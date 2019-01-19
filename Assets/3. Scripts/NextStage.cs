1a53285e11da300b8fe760cb11b045ab960abe47
songsunkook <songsunkook@gmail.com>
2019-01-19 22:45:05 +0900

    오디세이 ui 스타개수 생성 및 깃발 보완
    ui 스타개수생성은 구현했지만
    스타 획득은 구현하지 않은 상태.
    
    깃발 보완점
    스테이지6에서 깃발 진입시 오디세이로 진입

--- Assets/3. Scripts/NextStage.cs
+++ Assets/3. Scripts/NextStage.cs
@@ -11,8 +11,15 @@ public class NextStage : MonoBehaviour {
 
         char sceneNum = sceneName[sceneName.Length - 1];
 
-        sceneName = sceneName.Remove(sceneName.Length - 1);
+        if(sceneNum - '0' == 6)
+        {
+            SceneManager.LoadScene("Ch" + sceneName[2] + " Odyssey");
+        }
+        else
+        {
+            sceneName = sceneName.Remove(sceneName.Length - 1);
 
-        SceneManager.LoadScene(sceneName + (sceneNum + 1 - 48));
+            SceneManager.LoadScene(sceneName + (sceneNum + 1 - 48));
+        }
     }
 }
---
+++ Assets/3. Scripts/StarManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StarManager : MonoBehaviour
+{
+
+    Text text;
+
+    public static int starCount = 0;
+    int chapter;
+    int starFullCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        chapter = SceneManager.GetActiveScene().name[2] - '0';
+
+        starFullCount = chapter * 2 + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        text.text = starCount + " / " + starFullCount;
+    }
+}
