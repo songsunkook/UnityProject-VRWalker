b4680a95bc9e46dd562c350fafbcc2010316355a
songsunkook <songsunkook@gmail.com>
2019-01-19 23:31:09 +0900

    자동차 이동 시작딜레이 추가

--- Assets/3. Scripts/CarMove.cs
+++ Assets/3. Scripts/CarMove.cs
@@ -5,20 +5,29 @@ using UnityEditor;
 
 public class CarMove : MonoBehaviour
 {
+    public float firstDelay = 0f;
+
     public float speed = 7f;
+    
 
     public Transform[] wayPoints;
 
+    bool canMove = false;
     int turn = 0;
     Transform trans;
 
+    void MoveStart()
+    {
+        canMove = true;
+        trans.position = wayPoints[0].position;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         trans = GetComponent<Transform>();
-        trans.position = wayPoints[0].position;
-
+        trans.position = new Vector3(-100, -100, -100);
+        Invoke("MoveStart", firstDelay);
 
         for (int i = 0; i < wayPoints.Length; i++)
             wayPoints[i].gameObject.SetActive(false);
@@ -28,23 +37,25 @@ public class CarMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(trans.position == wayPoints[turn+1].position)
+        if (canMove)
         {
-            if (wayPoints.Length == turn + 2)
+
+            if (trans.position == wayPoints[turn + 1].position)
             {
-                turn = 0;
-                trans.position = wayPoints[0].position;
+                if (wayPoints.Length == turn + 2)
+                {
+                    turn = 0;
+                    trans.position = wayPoints[0].position;
+                }
+                else
+                    turn++;
             }
             else
-                turn++;
-        }
-        else
-        {
-            trans.LookAt(wayPoints[turn + 1]);
-            trans.position = Vector3.MoveTowards(trans.position, wayPoints[turn+1].position, speed * Time.deltaTime);
-        }
+            {
+                trans.LookAt(wayPoints[turn + 1]);
+                trans.position = Vector3.MoveTowards(trans.position, wayPoints[turn + 1].position, speed * Time.deltaTime);
+            }
 
-        
+        }
     }
 }
