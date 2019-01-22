5aa65e4e5e1b7f881914d8d1b756efd360d84cc3
songsunkook <songsunkook@gmail.com>
2019-01-23 00:27:36 +0900

    터치시 움직제어 구현(메인메뉴에서만)
    남은것
    1. 스테이지에서의 움직제어 구현
    (기존의 코드를 쓰되 게임시작을 안했을땐 못쓰게해야함)
    2. 일시정지 버튼동작안해서 막아놓음
    (현재 창이 뜨고 제거되는것까지만 만들어둠)

---
+++ Assets/3. Scripts/MagneticManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticManager : MonoBehaviour
+{
+    bool pressing = false;
+    float time = 0f;
+    float FullChargeTime = 2f;
+
+    public GameObject PausePanel;
+
+    private void TouchEnter()
+    {
+        pressing = true;
+    }
+
+    private void TouchExit()
+    {
+        //if(time < FullChargeTime)
+        if (pressing)
+            MoveCtrl.isStopped = !MoveCtrl.isStopped;
+        pressing = false;
+        time = 0f;
+    }
+
+    private void Pause()
+    {
+        //Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+    }
+
+    public void PauseExit()
+    {
+        //Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+    }
+
+    private void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            TouchEnter();
+        if (Input.GetMouseButtonUp(0))
+            TouchExit();
+
+        if (pressing)
+        {
+            time += Time.deltaTime;
+            if(time > FullChargeTime)
+            {
+                if (PausePanel.activeSelf == true)//포즈 해제 임시방편
+                    PauseExit();
+                else
+                    Pause();
+                time = 0f;
+                pressing = false;
+            }
+        }
+    }
+
+
+}
