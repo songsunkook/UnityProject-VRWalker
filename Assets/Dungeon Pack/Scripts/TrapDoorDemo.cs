da2f946041b3545e8f12b3604a1750bd7032314d
songsunkook <songsunkook@gmail.com>
2019-02-06 13:41:25 +0900

    트랩도어, 스피어 구현완료, 독극물 구현중

--- Assets/Dungeon Pack/Scripts/TrapDoorDemo.cs
+++ Assets/Dungeon Pack/Scripts/TrapDoorDemo.cs
@@ -8,11 +8,14 @@ public class TrapDoorDemo : MonoBehaviour {
 
     public Animator TrapDoorAnim; //Animator for the trap door;
 
+    private BoxCollider col;
+
     // Use this for initialization
     void Awake()
     {
         //get the Animator component from the trap;
         TrapDoorAnim = GetComponent<Animator>();
+        col = GetComponent<BoxCollider>();
         //start opening and closing the trap for demo purposes;
         StartCoroutine(OpenCloseTrap());
     }
@@ -22,10 +25,12 @@ public class TrapDoorDemo : MonoBehaviour {
     {
         //play open animation;
         TrapDoorAnim.SetTrigger("open");
+        col.enabled = false;
         //wait 2 seconds;
         yield return new WaitForSeconds(2);
         //play close animation;
         TrapDoorAnim.SetTrigger("close");
+        col.enabled = true;
         //wait 2 seconds;
         yield return new WaitForSeconds(2);
         //Do it again;
